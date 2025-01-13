24fa72ed1f7d42fefc4a5ab3074b352a05845bd2
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-01-13 16:23:55 -0700

    Update static config

--- light-cycle/svelte.config.js
+++ light-cycle/svelte.config.js
@@ -14,7 +14,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		appDir: 'app',
-        adapter: adapter(),
+        adapter: adapter(
+			{
+				fallback: 'index.html' 
+			}
+		),
 		paths: {
             base: dev ? '' : process.env.BASE_PATH,
         },
