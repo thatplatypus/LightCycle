198068d4ff7f6c90282a843e360f619a7d195dd1
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-01-13 16:17:36 -0700

    Static adapter for gh pages

--- light-cycle/svelte.config.js
+++ light-cycle/svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +13,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		appDir: 'app',
+        adapter: adapter(),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        },
 	}
 };
 
