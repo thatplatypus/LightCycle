ad27dcaf761f2bbb60860abecd29ea7b3f304e07
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-01-16 16:06:25 -0700

    Fix svelte config

--- light-cycle/svelte.config.js
+++ light-cycle/svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +8,6 @@ const config = {
 			base: process.env.NODE_ENV === 'production' ? '/LightCycle' : ''
 		}
 	},
-	preprocess: vitePreprocess()
 };
 
 export default config;
